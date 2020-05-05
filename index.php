ac04c6c65a2b401c4fdd1a1a89c9e1adba59c4e8
Victoire Lefebvre <guylaine.a@wanadoo.fr>
2020-05-05 17:19:47 +0200

    ex4 partie1 php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$name = 'guylaine'; //déclaration variable
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex1 partie1 php</title>
+</head>
+<body>
+<p>bonjour,
+  <?php
+  echo $name; //initialiser la valeur
+  ?>, comment vas tu?
+</p>
+
+</body>
+</html>
